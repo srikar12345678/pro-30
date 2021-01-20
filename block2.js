de2acae287a0b0b1d077d7604b9c9937237eb754
srikar12345678 <75025524+srikar12345678@users.noreply.github.com>
2021-01-20 21:52:12 +0530

    Add files via upload

---
+++ block2.js
@@ -0,0 +1,38 @@
+class Box {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.visibilty=255;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+    push();
+      if(this.body.speed>3){
+        World.remove(world,this.body);
+         this.visibilty=this.visibility-5;
+        tint(255,this.visibilty);
+        
+      }
+
+    else{
+        
+       // World.remove(world,this.body);
+       // push();
+       
+      
+     // pop();
+      
+      
+      rectMode(CENTER);
+      fill(" pink");
+      
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+    pop();
+    }
+  }
